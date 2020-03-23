2b921426f8a505fbc4e16a439cb4944df06b9fdd
calvinchankf <chan9118kin@gmail.com>
2020-03-23 13:59:36 +0800

    update: dependencies of react-react-router-redux

--- next-react-redux-antd/pages/index.js
+++ next-react-redux-antd/pages/index.js
@@ -1,10 +1,28 @@
 // components
-import Header from 'components/Header'
-import Footer from 'components/Footer'
+import Header from "components/Header";
+import Footer from "components/Footer";
+import { Select } from "antd";
+const { Option } = Select;
 // page component
 
-export default () => <div>
-  <Header />
-  <div>Hi there</div>
-  <Footer />
-</div>
+export default () => (
+	<div>
+		<Header />
+		<div>Hi there</div>
+		<Footer />
+		<Select defaultValue="lucy" style={{ width: 120 }} onChange={() => {}}>
+			<Option value="jack">Jack</Option>
+			<Option value="lucy">Lucy</Option>
+			<Option value="disabled" disabled>
+				Disabled
+			</Option>
+			<Option value="Yiminghe">yiminghe</Option>
+		</Select>
+		<Select defaultValue="lucy" style={{ width: 120 }} disabled>
+			<Option value="lucy">Lucy</Option>
+		</Select>
+		<Select defaultValue="lucy" style={{ width: 120 }} loading>
+			<Option value="lucy">Lucy</Option>
+		</Select>
+	</div>
+);
